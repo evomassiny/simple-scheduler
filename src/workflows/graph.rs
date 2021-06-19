7ed6d9bc5ec2630259cdb6f721320eeb9d317c53
evomassiny <hanzohatoryv@gmail.com>
2021-06-19 19:17:24 +0200

    test dependencies

--- src/workflows/graph.rs
+++ src/workflows/graph.rs
@@ -1,20 +1,10 @@
-use crate::workflows::parser::{
-    Info,
-    GenericInformation,
-    Parallel,
-    Argument,
-    Arguments,
-    StaticCommand,
-    NativeExecutable,
-    TaskRef,
-    Dependencies,
-    Task,
-    TaskFlow,
-    Job,
-    CLUSTER_ATTRIBUTE_NAME,
-};
-
 use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum WorkflowError {
+    TaskDoesNotExist,
+    DependencyCycle,
+}
 
 #[derive(Debug)]
 pub struct WorkFlowTask {
@@ -30,111 +20,28 @@ pub struct WorkFlowTask {
 #[derive(Debug)]
 pub struct WorkFlowGraph {
     pub name: String,
-    tasks: Vec<WorkFlowTask>,
+    pub tasks: Vec<WorkFlowTask>,
     /// indices of Task instances in `tasks`
-    dependency_indices: Vec<Vec<usize>>,
+    pub (crate) dependency_indices: Vec<Vec<usize>>,
+    /// index of each task (referenced by names) in `self.tasks`
+    pub (crate) name_to_idx: HashMap<String, usize>,
 }
 
 impl WorkFlowGraph {
 
-
-    pub fn from_str(workflow: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let job: Job = Job::from_str(workflow)?;
-
-        let mut dependency_indices: Vec<Vec<usize>> = Vec::new();
-        let mut tasks: Vec<WorkFlowTask> = Vec::new();
-
-        // first pass to collect the task names
-        let mut name_to_idx: HashMap<String, usize> = HashMap::new();
-        for (i, task) in job.task_flow.tasks.iter().enumerate() {
-            name_to_idx.insert(task.name.clone(), i);
+    /// returns references to the dependencies of `self`
+    pub fn get_task_dependencies<'a>(&'a self, task_name: &str) -> Result<Vec<&'a WorkFlowTask>, WorkflowError> {
+        let task_idx: usize = *self.name_to_idx
+            .get(task_name)
+            .ok_or(WorkflowError::TaskDoesNotExist)?;
+        let dependency_indices = self.dependency_indices
+            .get(task_idx)
+            .ok_or(WorkflowError::TaskDoesNotExist)?;
+        let mut tasks: Vec<&WorkFlowTask> = Vec::new();
+        for dep_idx in dependency_indices {
+            tasks.push(&self.tasks[*dep_idx]);
         }
-
-        // Creates tasks
-        for (i, task) in job.task_flow.tasks.iter().enumerate() {
-            // collect dependencies
-            let mut dependencies: Vec<usize> = Vec::new();
-            if let Some(deps) = &task.dependencies {
-                for dep in &deps.tasks {
-                    if let Some(task_idx) = name_to_idx.get(&dep.task_ref) {
-                        dependencies.push(*task_idx);
-                    }
-                }
-            }
-            dependency_indices.push(dependencies);
-            
-            // collect node_count
-            let mut node_count = 0;
-            if let Some(parallel_info) = &task.parallel {
-                node_count = parallel_info.numberOfNodes;
-            }
-            // collect cluster_name
-            let mut cluster_name: Option<String> = None;
-            for info in &task.genericInformation.infos {
-                if info.name == CLUSTER_ATTRIBUTE_NAME {
-                    cluster_name = Some(info.value.clone());
-                }
-            }
-            // collect executable
-            let executable = task.executable.command.value.clone();
-            let arguments = task.executable
-                .command
-                .arguments
-                .arguments
-                .iter().map(|a| a.value.clone())
-                .collect::<Vec<String>>();
-            
-            tasks.push(
-                WorkFlowTask {
-                    name: task.name.clone(),
-                    cluster_name: cluster_name,
-                    node_count: node_count,
-                    executable: executable,
-                    executable_arguments: arguments,
-                }
-            )
-        }
-
-        Ok(Self {
-            name: job.name,
-            tasks: tasks,
-            dependency_indices: dependency_indices,
-        })
+        Ok(tasks)
     }
-}
 
-#[test]
-fn test_deserialization() {
-    const JOB_STR: &str = r#"<?xml version="1.0"?>
-<job name="job-name">
-  <taskFlow>
-    <task name="A" runAsMe="true">
-      <genericInformation>
-        <info name="NODE_ACCESS_TOKEN" value="cluster_name_1"/>
-      </genericInformation>
-      <parallel numberOfNodes="20">
-        <topology>
-          <bestProximity/>
-        </topology>
-      </parallel>
-      <nativeExecutable>
-        <staticCommand value="executable_name">
-          <arguments>
-            <argument value="argument_1"/>
-            <argument value="argument_2"/>
-          </arguments>
-        </staticCommand>
-      </nativeExecutable>
-    </task>
-  </taskFlow>
-</job>"#;
-
-    let taskflow = WorkFlowGraph::from_str(JOB_STR).unwrap();
-    assert_eq!(taskflow.name, String::from("job-name"));
-    assert_eq!(taskflow.tasks[0].name, String::from("A"));
-    assert_eq!(taskflow.tasks[0].cluster_name, Some("cluster_name_1".into()));
-    assert_eq!(taskflow.tasks[0].node_count, 20);
-    assert_eq!(taskflow.tasks[0].executable, String::from("executable_name"));
-    assert_eq!(taskflow.tasks[0].executable_arguments[0], String::from("argument_1"));
-    assert_eq!(taskflow.tasks[0].executable_arguments[1], String::from("argument_2"));
 }
--- src/workflows/parser.rs
+++ src/workflows/parser.rs
@@ -1,5 +1,9 @@
 use serde::Deserialize;
 use quick_xml::de::{from_str, from_reader, DeError};
+use crate::workflows::graph::{
+    WorkFlowGraph,WorkFlowTask
+};
+use std::collections::HashMap;
 
 pub const CLUSTER_ATTRIBUTE_NAME: &str = "NODE_ACCESS_TOKEN";
 
@@ -69,7 +73,7 @@ pub struct Task {
     pub name: String,
     pub runAsMe: bool,
     pub genericInformation: GenericInformation,
-    pub dependencies: Option<Dependencies>,
+    pub depends: Option<Dependencies>,
     pub parallel: Option<Parallel>,
     #[serde(rename = "nativeExecutable")]
     pub executable: NativeExecutable,
@@ -95,6 +99,78 @@ impl Job {
     }
 }
 
+
+impl WorkFlowGraph {
+
+
+    /// Build a WorkFlowTask from an XML string representation.
+    /// NOTE: 
+    /// does not check for dependency cycles.
+    pub fn from_str(workflow: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let job: Job = Job::from_str(workflow)?;
+
+        let mut dependency_indices: Vec<Vec<usize>> = Vec::new();
+        let mut tasks: Vec<WorkFlowTask> = Vec::new();
+
+        // first pass to collect the task names
+        let mut name_to_idx: HashMap<String, usize> = HashMap::new();
+        for (i, task) in job.task_flow.tasks.iter().enumerate() {
+            name_to_idx.insert(task.name.clone(), i);
+        }
+
+        // Creates tasks
+        for (i, task) in job.task_flow.tasks.iter().enumerate() {
+            // collect dependencies
+            let mut dependencies: Vec<usize> = Vec::new();
+            if let Some(deps) = &task.depends {
+                for dep in &deps.tasks {
+                    if let Some(task_idx) = name_to_idx.get(&dep.task_ref) {
+                        dependencies.push(*task_idx);
+                    }
+                }
+            }
+            dependency_indices.push(dependencies);
+            
+            // collect node_count
+            let mut node_count = 0;
+            if let Some(parallel_info) = &task.parallel {
+                node_count = parallel_info.numberOfNodes;
+            }
+            // collect cluster_name
+            let mut cluster_name: Option<String> = None;
+            for info in &task.genericInformation.infos {
+                if info.name == CLUSTER_ATTRIBUTE_NAME {
+                    cluster_name = Some(info.value.clone());
+                }
+            }
+            // collect executable
+            let executable = task.executable.command.value.clone();
+            let arguments = task.executable
+                .command
+                .arguments
+                .arguments
+                .iter().map(|a| a.value.clone())
+                .collect::<Vec<String>>();
+            
+            tasks.push(
+                WorkFlowTask {
+                    name: task.name.clone(),
+                    cluster_name: cluster_name,
+                    node_count: node_count,
+                    executable: executable,
+                    executable_arguments: arguments,
+                }
+            )
+        }
+
+        Ok(Self {
+            name: job.name,
+            tasks: tasks,
+            dependency_indices: dependency_indices,
+            name_to_idx: name_to_idx,
+        })
+    }
+}
 #[test]
 fn test_deserialization() {
     const JOB_STR: &str = r#"<?xml version="1.0"?>
@@ -124,5 +200,49 @@ fn test_deserialization() {
     let job: Job = Job::from_str(JOB_STR).unwrap();
     assert_eq!(job.name, String::from("job-name"));
     assert_eq!(job.task_flow.tasks[0].name, String::from("A"));
-    eprintln!("{:#?}", job);
+}
+
+#[test]
+fn test_single_task_parsing() {
+    const JOB_STR: &str = r#"<?xml version="1.0"?>
+<job name="job-name">
+  <taskFlow>
+    <task name="A" runAsMe="true">
+      <genericInformation>
+        <info name="NODE_ACCESS_TOKEN" value="cluster_name_1"/>
+      </genericInformation>
+      <parallel numberOfNodes="20">
+        <topology>
+          <bestProximity/>
+        </topology>
+      </parallel>
+      <nativeExecutable>
+        <staticCommand value="executable_name">
+          <arguments>
+            <argument value="argument_1"/>
+            <argument value="argument_2"/>
+          </arguments>
+        </staticCommand>
+      </nativeExecutable>
+    </task>
+  </taskFlow>
+</job>"#;
+
+    let taskflow = WorkFlowGraph::from_str(JOB_STR).unwrap();
+    assert_eq!(taskflow.name, String::from("job-name"));
+    assert_eq!(taskflow.tasks[0].name, String::from("A"));
+    assert_eq!(taskflow.tasks[0].cluster_name, Some("cluster_name_1".into()));
+    assert_eq!(taskflow.tasks[0].node_count, 20);
+    assert_eq!(taskflow.tasks[0].executable, String::from("executable_name"));
+    assert_eq!(taskflow.tasks[0].executable_arguments[0], String::from("argument_1"));
+    assert_eq!(taskflow.tasks[0].executable_arguments[1], String::from("argument_2"));
+}
+
+#[test]
+fn test_several_tasks_parsing() {
+    const JOB_STR: &str = include_str!("../../test-data/workflow.xml");
+    let taskflow = WorkFlowGraph::from_str(JOB_STR).unwrap();
+    let C_deps: Vec<&WorkFlowTask> = taskflow.get_task_dependencies("C").unwrap();
+    // check dependencies
+    assert_eq!(C_deps[0].name, String::from("A"));
 }
