cac2487f898d69f20fdf0dbe9ff6d99728671d8f
evomassiny <hanzohatoryv@gmail.com>
2021-07-07 22:46:25 +0200

    Added model test

--- src/models.rs
+++ src/models.rs
@@ -10,7 +10,7 @@ pub enum ModelError {
     InvalidTaskName,
     DependencyCycle,
     ModelNotFound,
-    DBError,
+    DBError(String),
 }
 
 /// trait shared by Database models,
@@ -104,7 +104,7 @@ impl Model for Job {
             .bind(self.id.ok_or(ModelError::ModelNotFound)?)
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         Ok(())
     }
 
@@ -115,7 +115,7 @@ impl Model for Job {
             .bind(self.status.as_u8())
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         let id = query_result.last_insert_rowid();
         self.id = Some(id);
         Ok(())
@@ -168,12 +168,12 @@ impl Model for Task {
             .bind(self.id.ok_or(ModelError::ModelNotFound)?)
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         Ok(())
     }
 
     async fn create(&mut self, conn: &mut SqliteConnection) -> Result<(), ModelError>{
-        let query_result = sqlx::query("INSERT INTO jobs (name, status, handle, command, job) VALUES (?, ?, ?, ?, ?)")
+        let query_result = sqlx::query("INSERT INTO tasks (name, status, handle, command, job) VALUES (?, ?, ?, ?, ?)")
             .bind(&self.name)
             .bind(self.status.as_u8())
             .bind(&self.handle)
@@ -181,7 +181,7 @@ impl Model for Task {
             .bind(&self.job)
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         let id = query_result.last_insert_rowid();
         self.id = Some(id);
         Ok(())
@@ -226,7 +226,7 @@ impl Model for TaskDependency {
             .bind(self.id.ok_or(ModelError::ModelNotFound)?)
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         Ok(())
     }
 
@@ -236,7 +236,7 @@ impl Model for TaskDependency {
             .bind(&self.parent)
             .execute(conn)
             .await
-            .map_err(|_| ModelError::DBError)?;
+            .map_err(|e| ModelError::DBError(format!("{:?}", e)))?;
         let id = query_result.last_insert_rowid();
         self.id = Some(id);
         Ok(())
@@ -265,7 +265,7 @@ pub struct Batch {
 
 
 impl Batch {
-    pub fn from_graph(workflow: &WorkFlowGraph, conn: &mut SqliteConnection) -> Result<Self, ModelError> {
+    pub async fn from_graph(workflow: &WorkFlowGraph, conn: &mut SqliteConnection) -> Result<Self, ModelError> {
         // validate input
         if !workflow.are_task_names_unique() {
             return Err(ModelError::InvalidTaskName);
@@ -276,7 +276,7 @@ impl Batch {
 
         // Create and save Job
         let mut job = Job::new(&workflow.name);
-        job.save(conn);
+        let _ = job.save(conn).await?;
         let job_id: i64 = job.id.ok_or(ModelError::ModelNotFound)?;
 
         // create and save all tasks
@@ -290,7 +290,7 @@ impl Batch {
                 command: graph_task.command(),
                 job: job_id,
             };
-            task.save(conn);
+            let _ = task.save(conn).await?;
             tasks.push(task);
         }
 
@@ -301,7 +301,7 @@ impl Batch {
             for dep_idx in deps_ids {
                 let parent: i64 = tasks[*dep_idx].id().ok_or(ModelError::ModelNotFound)?;
                 let mut dependency = TaskDependency { id: None, child, parent };
-                dependency.save(conn);
+                let _ = dependency.save(conn).await?;
                 dependencies.push(dependency);
             }
         }
@@ -310,3 +310,75 @@ impl Batch {
 
 }
 
+#[cfg(test)]
+mod test {
+    use crate::workflows::{WorkFlowGraph,WorkFlowTask,WorkflowError};
+    use sqlx::{SqliteConnection,Connection,Executor};
+    use rocket::tokio;
+    use std::collections::HashMap;
+    use crate::models::{Model, Batch, Job, Task, TaskDependency, Status, ModelError};
+
+    /// create an in-memory database, and execute the initial migration.
+    async fn setup_in_memory_database() -> Result<SqliteConnection, Box<dyn std::error::Error>> {
+        let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+        conn.execute(include_str!("../migrations/20210402155322_creation.sql")).await?;
+        Ok(conn)
+    }
+
+    /// build a test graph With 2 tasks A and B, with B depending of A
+    fn build_test_workflow_graph() -> WorkFlowGraph {
+        WorkFlowGraph {
+            name: "test-job".to_string(),
+            tasks: vec![
+                WorkFlowTask {
+                    name: "A".to_string(),
+                    cluster_name: None,
+                    node_count: 1,
+                    executable: "touch".to_string(),
+                    executable_arguments: vec!["/tmp/tmp-empty-file".to_string()],
+                },
+                WorkFlowTask {
+                    name: "B".to_string(),
+                    cluster_name: None,
+                    node_count: 1,
+                    executable: "rm".to_string(),
+                    executable_arguments: vec!["/tmp/tmp-empty-file".to_string()],
+                },
+            ],
+            dependency_indices: vec![vec![], vec![0]],
+            name_to_idx: {
+                let mut name_to_idx: HashMap<String, usize> = HashMap::new();
+                name_to_idx.insert("A".to_string(), 0);
+                name_to_idx.insert("B".to_string(), 1);
+                name_to_idx
+            }
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_graph_persistence() {
+        // build DB
+        let mut conn = setup_in_memory_database().await.unwrap();
+        // build graph
+        let graph = build_test_workflow_graph();
+        assert!(graph.is_cycle_free());
+        assert!(graph.are_task_names_unique());
+        
+        // test batch creation
+        let batch = Batch::from_graph(&graph, &mut conn).await.expect("Failed to build Batch");
+        assert_eq!(&batch.job.name, "test-job");
+
+        // test (loosely) the model creation in the database
+        let (job_name,): (String,) = sqlx::query_as("SELECT name FROM jobs")
+            .fetch_one(&mut conn).await.expect("failed to count tasks");
+        assert_eq!(&job_name, &batch.job.name);
+
+        let (task_count,): (i64,) = sqlx::query_as("SELECT count(id) FROM tasks")
+            .fetch_one(&mut conn).await.expect("failed to count tasks");
+        assert_eq!(task_count, 2);
+
+        let (dep_count,): (i64,) = sqlx::query_as("SELECT count(id) FROM task_dependencies")
+            .fetch_one(&mut conn).await.expect("failed to count tasks");
+        assert_eq!(dep_count, 1);
+    }
+}
