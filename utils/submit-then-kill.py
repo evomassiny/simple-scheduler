c3e3706f8513da2279b579d19cf7634bcdb1555d
evomassiny <hanzohatoryv@gmail.com>
2021-09-06 16:30:56 +0200

    added debug script

---
+++ utils/submit-then-kill.py
@@ -0,0 +1,62 @@
+import os.path
+from pprint import pprint
+from requests import Session
+from time import sleep
+
+repo_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# dummy
+cred_path = os.path.join(repo_directory, "utils", "auth-test.py")
+
+print("mockup login")
+session = Session()
+with open(cred_path, "rb") as cred_fd:
+    session.post(
+        "http://127.0.0.1:8000/rest/scheduler/login",
+        files={"credential": ("creds.enc", cred_fd, "text/plain")},
+    )
+
+# submit job
+workflow_path = os.path.join(repo_directory, "utils", "echo-workflow.xml")
+print("submit", workflow_path)
+with open(workflow_path, "rb") as workflow_fd:
+    response = session.post(
+        "http://127.0.0.1:8000/rest/scheduler/submit/",
+        files={"file": ("workflow.xml", workflow_fd, "application/xml")},
+    )
+    data = response.json()
+    pprint(data)
+    print()
+
+
+job_id = data['id']
+print("get status")
+for _ in range(4):
+    response = session.get(
+        "http://127.0.0.1:8000/rest/scheduler/jobs/{job_id:d}".format(job_id=job_id),
+    )
+    data = response.json()
+    pprint(data)
+    sleep(0.5)
+    print()
+
+
+
+print("Killing job")
+response = session.put(
+    "http://127.0.0.1:8000/rest/scheduler/jobs/{job_id:d}/kill".format(job_id=job_id),
+)
+if response.status_code == 200:
+    data = response.json()
+    pprint(data)
+else:
+    print("Failed:")
+    print(response.text)
+print()
+
+print("get status")
+response = session.get(
+    "http://127.0.0.1:8000/rest/scheduler/jobs/{job_id:d}".format(job_id=job_id),
+)
+data = response.json()
+pprint(data)
